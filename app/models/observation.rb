57adbd14ec134a06222b1ca9cfe71932e5671b5c
Mark Brettin <mark@jackrabbitsystems.com>
2018-06-10 14:30:00 -0600

    Observation association fix

--- app/models/observation.rb
+++ app/models/observation.rb
@@ -1,4 +1,4 @@
 class Observation < ApplicationRecord
   has_many :admissions_observations
-  has_many :observations, through: :admissions_observations
+  has_many :admissions, through: :admissions_observations
 end
