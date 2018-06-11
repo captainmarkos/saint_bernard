f16435d52ab67bb55fe3dcbfa87cb9dcafe85932
Mark Brettin <mark@jackrabbitsystems.com>
2018-06-11 05:33:58 -0600

    association fix

--- app/models/medication_order.rb
+++ app/models/medication_order.rb
@@ -1,4 +1,4 @@
 class MedicationOrder < ApplicationRecord
   has_many :patients_medication_orders
-  has_many :medication_orders, through: :patients_medication_orders
+  has_many :patients, through: :patients_medication_orders
 end
--- app/models/treatment.rb
+++ app/models/treatment.rb
@@ -1,4 +1,4 @@
 class Treatment < ApplicationRecord
   has_many :patients_treatments
-  has_many :treatments, through: :patients_treatments
+  has_many :patients, through: :patients_treatments
 end
